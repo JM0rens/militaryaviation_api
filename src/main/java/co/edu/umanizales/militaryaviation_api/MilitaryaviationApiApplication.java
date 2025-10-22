d8721e0f6facf768bd455379dc5ecbbd8b44b788
JM0rens <juandros123@gmail.com>
2025-10-22 15:45:28 -0500

    My first version api MilitaryAviation with greeting hello champions

---
+++ src/main/java/co/edu/umanizales/militaryaviation_api/MilitaryaviationApiApplication.java
@@ -0,0 +1,13 @@
+package co.edu.umanizales.militaryaviation_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MilitaryaviationApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MilitaryaviationApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/co/edu/umanizales/militaryaviation_api/MilitaryaviationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.umanizales.militaryaviation_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MilitaryaviationApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
